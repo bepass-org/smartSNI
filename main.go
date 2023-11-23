c45aede6f4d9189cf9f0f9774e1fc1a2ba2546a0
uoosef <ghobadi@yekaan.ir>
2023-11-23 22:49:20 +0330

    memory optimization

--- main.go
+++ main.go
@@ -209,14 +209,9 @@ type readOnlyConn struct {
 	reader io.Reader
 }
 
-func (conn readOnlyConn) Read(p []byte) (int, error)  { return conn.reader.Read(p) }
-func (conn readOnlyConn) Write(p []byte) (int, error) { return 0, io.ErrClosedPipe }
-func (conn readOnlyConn) Close() error {
-	if tcpConn, ok := conn.reader.(io.Closer); ok {
-		return tcpConn.Close()
-	}
-	return nil
-}
+func (conn readOnlyConn) Read(p []byte) (int, error)         { return conn.reader.Read(p) }
+func (conn readOnlyConn) Write(p []byte) (int, error)        { return 0, io.ErrClosedPipe }
+func (conn readOnlyConn) Close() error                       { return conn.Close() }
 func (conn readOnlyConn) LocalAddr() net.Addr                { return nil }
 func (conn readOnlyConn) RemoteAddr() net.Addr               { return nil }
 func (conn readOnlyConn) SetDeadline(t time.Time) error      { return nil }
